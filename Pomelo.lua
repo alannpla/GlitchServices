ec600f8db7f76bb5f8653e0d01a1e875ef427e84
Alan Plaza <71902427+alannpla@users.noreply.github.com>
2023-05-04 14:39:15 -0300

    0.39
    actualizacion transaction error

--- Pomelo.lua
+++ Pomelo.lua
@@ -4,7 +4,7 @@ util.require_natives(1651208000)
 ---------------AUTO ACTUALIZACION
 
 local response = false
-local localVer = 0.38
+local localVer = 0.39
 local localKs = false
 
 util.toast("Hola, " .. players.get_name(players.user()))
@@ -36,7 +36,7 @@ repeat
     util.yield()
 until response
 
-menu.divider(menu.my_root(), "0.38", {}, end)
+menu.divider(menu.my_root(), "0.39", {}, end)
 ---------------LOCALIZACIONES
 
 function teleportToAirport()
@@ -207,7 +207,7 @@ menu.divider(menu.my_root(), "Test", {}, end)
 local recovery = menu.list(menu.my_root(), "Recovery", {}, "ALERTA! Todas las opciones de esta carpeta se consideran riesgosas. No nos hacemos responsables. Estas advertido.")
 
 
-menu.toggle_loop(recovery, "Disable Transaction Errors", {"hcnotransactionerr"}, "This can be used to remove transaction errors while you are doing special cargo money loop in Musiness Banager Lua.", function()
+menu.toggle_loop(recovery, "Deshabilitar Error de transaccion", {}, "Esto se puede usar para eliminar errores de transacción.", function()
             if IS_WORKING(false) ~= "" then return end
             if not util.is_session_started() then return end
 
